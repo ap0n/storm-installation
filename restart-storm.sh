bb6768b2a0faf5bb640592b0768a459c00071bdf
Apostolos Nydriotis <anydriotis@softnet.tuc.gr>
2015-02-18 09:45:48 +0200

    Add logviewer to slave-supervisord.conf add restart-storm script

---
+++ restart-storm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for host in snf-master snf-slave0 snf-slave1 snf-slave2 snf-slave3 snf-slave4 snf-slave5 snf-slave6
+do
+	ssh user@$host 'sudo service supervisor stop'
+	ssh user@$host 'sudo service supervisor start'
+	echo $host ok
+done
